3db481c7cad752f2edc96e856591883115a6a40d
TOXYGENCY <delorean2026@ya.ru>
2026-01-11 18:08:56 +0300

    feat(диплом): заполнение 2х листов (из 3х) - Сделано заполнение листов "Диплом" и "Обладатель диплома" данными - Добавлены вспомогательные методы заполнения ячеек - При обработке ячеек к ним добавляется комментарий, который визуально помечает ячейку в Excel, для перепроверки результата работы

--- Dekauto.Export.Service/Domain/Services/DiplomaSupplementExportService.cs
+++ Dekauto.Export.Service/Domain/Services/DiplomaSupplementExportService.cs
@@ -1,6 +1,7 @@
 ﻿using Dekauto.Export.Service.Domain.Entities.DTO;
 using Dekauto.Export.Service.Domain.Interfaces;
 using OfficeOpenXml;
+using System.Globalization;
 
 namespace Dekauto.Export.Service.Domain.Services
 {
@@ -8,19 +9,62 @@ namespace Dekauto.Export.Service.Domain.Services
     {
         private IConfiguration _configuration;
         private readonly ILogger<DiplomaSupplementExportService> _logger;
+        private readonly string cellComment = "Значение изначально было установлено автоматически (с помощью Dekauto).";
+        private readonly string commentAuthor = "Dekauto";
+        private ExcelWorksheet? activeWorksheet;
 
         public DiplomaSupplementExportService(IConfiguration configuration,
             ILogger<DiplomaSupplementExportService> logger)
         {
             _configuration = configuration;
             _configuration.GetRequiredSection("ExportDiploma");
-            this._logger = logger;
+            cellComment = _configuration.GetValue<string>("ExportCommentText");
+            commentAuthor = _configuration.GetValue<string>("ExportCommentAuthor");
+            _logger = logger;
 
+        }
+
+        private void AddComment(string cell)
+        {
+            activeWorksheet.Cells[cell].AddComment(cellComment, commentAuthor);
+        }
+
+        private void SetCellValue(string cell, object value)
+        {
+            object cellValue;
+
+            // Проверяем сначала DateOnly (это сработает и для DateOnly? с значением)
+            if (value is DateOnly dateOnly)
+            {
+                cellValue = FormatDate(dateOnly);
+            }
+            // Затем проверяем на null
+            else if (value is null)
+            {
+                cellValue = null;
+                _logger.LogWarning($"{cell} has received a null value.");
+            }
+            else
+            {
+                cellValue = value;
+            }
+
+            activeWorksheet.Cells[cell].Value = cellValue;
+            AddComment(cell);
+
+            _logger.LogTrace($"Для ячейки {cell} установлено значение \"{cellValue}\"");
+        }
+
+        private string FormatDate(DateOnly date)
+        {
+            var culture = new CultureInfo("ru-RU");
+            return date.ToString("d MMMM yyyy 'года'", culture);
         }
 
         public async Task<(MemoryStream, string)> ExportDiplomaSupplement(DiplomaSupplementExportRequest request)
         {
             ArgumentNullException.ThrowIfNull(request);
+            activeWorksheet = null;
 
             var (templatePath, e1, m1) = ChooseTemplateFile(request.manufacturer, request.educationLevel);
             _logger.LogInformation($"Найден файл шаблона: {m1}, {e1}");
@@ -73,14 +117,18 @@ namespace Dekauto.Export.Service.Domain.Services
         {
             if (package.Workbook.Worksheets.Count == 0)
                 throw new InvalidOperationException("Файл шаблона не содержит листов");
+            activeWorksheet = null;
 
             var ws = package.Workbook.Worksheets;
             var diplomaSheet = ws.Where(w => w.Name.Trim()
                 .Contains("диплом", StringComparison.OrdinalIgnoreCase)) // ищем "Диплом"
-                .MinBy(w => w.Name);
-            var ownerSheet = ws.Where(w => w.Name.Trim().Contains("обладат", StringComparison.OrdinalIgnoreCase)).First(); // ищем "1 Обладатель диплома"
-            var programMasteringSheet = ws.Where(w => w.Name.Trim().Contains("програм", StringComparison.OrdinalIgnoreCase)).First(); // ищем "3 Освоение программы"
-            //var otherDataSheet = ws.Where(w => w.Name.Trim().Contains("сведен", StringComparison.OrdinalIgnoreCase)); // ищем "4 доп.сведения"
+                .MinBy(w => w.Name.Length);
+            var ownerSheet = ws.Where(w => w.Name.Trim().Contains("обладат", StringComparison.OrdinalIgnoreCase))
+                .First(); // ищем "1 Обладатель диплома"
+            var programMasteringSheet = ws.Where(w => w.Name.Trim().Contains("програм", StringComparison.OrdinalIgnoreCase))
+                .First(); // ищем "3 Освоение программы"
+            //var otherDataSheet = ws.Where(w => w.Name.Trim().Contains("сведен", StringComparison.OrdinalIgnoreCase))
+            //  .First(); // ищем "4 доп.сведения"
 
             _logger.LogInformation($"Начинаем заполнение страницы диплома (лист \"{diplomaSheet.Name}\")...");
             FillDiplomaSheet(diplomaSheet, data);
@@ -101,17 +149,37 @@ namespace Dekauto.Export.Service.Domain.Services
 
         private void FillDiplomaSheet(ExcelWorksheet sheet, DiplomaSupplementData data)
         {
+            activeWorksheet = sheet;
 
+            if (data is null || data.DiplomaWithHonors is null)
+            {
+                _logger.LogWarning("Статус диплома \"с отличием\" не найден, пропускаем...");
+                return;
+            }
+
+            SetCellValue("B6", data.DiplomaWithHonors.Value ? "с отличием" : null);
+
+            activeWorksheet = null;
         }
 
         private void FillOwnerSheet(ExcelWorksheet sheet, DiplomaSupplementData data)
         {
+            activeWorksheet = sheet;
 
+            SetCellValue("B3", data.Surname);
+            SetCellValue("B4", data.Name);
+            SetCellValue("B5", data.Patronymic);
+            SetCellValue("B6", data.BirthdayDate);
+            SetCellValue("B12", data.DiplomaWithHonors.Value ? "с отличием" : null);
+
+            activeWorksheet = null;
         }
 
         private void FillProgramMasteringSheet(ExcelWorksheet sheet, DiplomaSupplementData data)
         {
+            activeWorksheet = sheet;
 
+            activeWorksheet = null;
         }
     }
 }
